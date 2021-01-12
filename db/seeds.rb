7828d5d7622c927a189bba291bd771cd41afd18c
Julian Segrestan <julian.nihon@gmail.com>
2021-01-12 20:59:33 +0900

    added routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Erasing db ..."
+Restaurant.destroy_all
+
+25.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: Restaurant::CATEGORIES.sample
+  )
+
+  puts Restaurant.last.name
+end
+
+puts "Finished ..."
